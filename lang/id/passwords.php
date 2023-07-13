da49ef5a3b2ca0873eeb7449c2842be9750271aa
hakim-asrori <saneglos005@gmail.com>
2023-07-13 18:29:20 +0700

    setup language

---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'kata sandi anda sudah direset.',
+    'sent' => 'kami sudah mengirim email yang berisi tautan untuk mereset kata sandi anda!',
+    'throttled' => 'harap tunggu sebelum mencoba lagi.',
+    'token' => 'token pengaturan ulang kata sandi tidak sah.',
+    'user' => "kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
+
+];
